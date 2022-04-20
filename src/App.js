8320765aabeddab84d872c4e040d40a63b766931
Akshat kumar <theyashgreat@gmail.com>
2022-04-20 19:27:01 +0530

    Axios_Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [username,setUsername]=useState('');
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  console.log({email,password});
+  const headers = {
+    
+  }
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+  // axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+  axios.post("https://reqres.in/api/login",{
+        email: email,
+        password: password
+      })
+      .then(result=>{
+        console.log(result)
+      })
+      .catch(error=>{
+        console.log(error)
+      })
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="container">
+       
+  
+       <form >
+         <h1>Login Form</h1>
+         <hr/>
+         <div className="ui divider"></div>
+         <div className="ui form">
+           <div className="field">
+             <label>Username</label><br></br>
+             <input
+               type="text"
+               name="username"
+               placeholder="Username"
+               value={username}
+               onChange={e => setUsername(e.target.value)}
+             />
+           </div>
+           <div className="field">
+             <label>Email</label><br />
+             <input
+               type="text"
+               name="email"
+               placeholder="Email"
+               value={email}
+               onChange={e => setEmail(e.target.value)}
+
+             />
+           </div>
+           <div className="field">
+             <label>Password</label><br />
+             <input
+               type="password"
+               name="password"
+               placeholder="Password"
+                value={password}
+               onChange={e => setPassword(e.target.value)}
+
+             />
+           </div>
+           <button className="btn" onClick={handleSubmit}>Submit</button>
+         </div>
+       </form>
+     </div>
+    </>
   );
 }
 
